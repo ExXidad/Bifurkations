98473b17ad8ff5bf0f0f8a8b3a6559475ea7adb0
Xidad <kolesnikov.iv@gmail.com>
2021-02-13 09:19:50 +0300

    second

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iomanip>
 #include <string>
+#include <cmath>
 
 double xNextF(const double &x, const double &r)
 {
@@ -16,28 +17,21 @@ int main()
 		std::fstream file;
 		file.open("../results/" + std::to_string(x0n), std::ios::out);
 
-		for (double r = 0; r <= 1; r += 0.0005)
+		for (double r = 0; r <= 1; r += 0.0001)
 		{
-			double xPrev = 0;
 			double x = x0n * x0Step;
 			double xNext = xNextF(x, r);
 
-			double eps = x * 0.0001;
 			bool converged = false;
 
-			for (int n = 0; n < 10000; ++n)
+			for (int n = 0; n < 1000; ++n)
 			{
-				xPrev = x;
 				x = xNext;
 				xNext = xNextF(x, r);
-//			std::cout << xPrev << " " << x << " " << xNext << std::endl;
 
-				if ((xNext - x) - (x - xPrev) < eps && false)
-				{
-					file << r << "\t" << xNext << std::endl;
-					converged = true;
-					break;
-				}
+				file << r << "\t" << xNext << std::endl;
+				converged = true;
+
 			}
 
 			file << r << "\t" << xNext << std::endl;
